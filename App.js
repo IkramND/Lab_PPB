cee9bc8487efcd884e1c4c3f36e33af42c6a50b2
IkramND <113400552+IkramND@users.noreply.github.com>
2024-04-19 11:42:26 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,26 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Car from './components/Car';
-import Motor from './components/Motor';
-import Bicycle from './components/Bicycle';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Text, TouchableOpacity, View } from "react-native";
 
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator()
+
   return (
-    <View style={styles.container}>
-      <Car/>
-      <Motor/>
-      <Bicycle/>
-      <StatusBar style="auto" />
-    </View>
+    
+    <NavigationContainer>
+
+      <Tab.NavigatorContainer>
+
+        <Tab.Screen name="page1" component={page1}/>
+        <Tab.Screen name="page2" component={page2}/>
+
+      </Tab.NavigatorContainer>
+{/* <Stack.Navigator>
+  <Stack.Screen name="page1" component={page1} options={{ title: "Welcome" }}/>
+  <Stack.Screen  name="page2" component={page2}/>
+
+  
+</Stack.Navigator> */}
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF5EE ',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const page1 = ({navigation}) => {
+const page1 = () => {
+
+  return<Text>page 1</Text>;
+  // return (
+  //   <TouchableOpacity onPress={() => navigation.navigate("page2",{name:"Gymnastiar"})}>
+  //     <View style={{ backgroundColor : "purple",padding:10,borderRadius:10, margin:10}}>
+
+  //       <Text style = {{color : "white", textAlign:"center"}}>Click here to see me</Text>
+  //     </View>
+
+  //   </TouchableOpacity>
+  // );
+};
+
+
+const page2= () => {
+  return<Text>page 2</Text>;
+};
+// const page2 = ({ route }) => {
+//   retu
+//   // return <Text>This is {route.params.name}</Text>
+// };
